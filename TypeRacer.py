e048291f9760da8c5cfc06d2bfefb11133f0e35a
Chase <100978029+ChaseRocks360@users.noreply.github.com>
2024-06-06 02:27:16 -0500

    TypeRacer.py

---
+++ TypeRacer.py
@@ -0,0 +1,39 @@
+# Imports
+import time
+from selenium import webdriver
+import keyboard
+import pyautogui
+
+
+# Defining Opera Drivers Path
+driver = webdriver.operagx(operagxdriver-0.10)
+
+
+# Opening Website
+driver.get("https://play.typeracer.com/")
+time.sleep(5)
+
+# Playing Game
+keyboard.press_and_release('ctrl+alt+i')
+time.sleep(2)
+
+# Getting Text
+text = driver.find_element_by_class_name("gameView").text
+text = text.split("\n")
+print(type(text))
+print(text[-3])
+
+# Sleeps Before Typing
+time.sleep(15)
+
+# Types Text
+pyautogui.typewrite(text[-3],0.01)
+
+# Sleeps For Sometime And Quits
+time.sleep(15)
+driver.quit()
+
+
+
+
+
